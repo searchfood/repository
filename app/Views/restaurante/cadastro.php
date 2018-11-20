e680dbb2dfdc201073cb293b5c9294ba8c55befa
Danilo Gonçalves <danilodesenvolvedorweb@hotmail.com>
2018-11-20 15:08:58 -0200

    View Login criada

--- app/Views/restaurante/cadastro.php
+++ app/Views/restaurante/cadastro.php
@@ -11,12 +11,12 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Copse" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Niramit" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
         <!-- Fontawesome 5 -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-        <!-- CSS Template -->
+        <!-- CSS Template and Others -->
         <link rel="stylesheet" href="<?=URL?>/public/css/template.css">
 
         <!-- Bootstrap CSS -->
--- app/Views/restaurante/login.php
+++ app/Views/restaurante/login.php
@@ -16,17 +16,56 @@
         <!-- Fontawesome 5 -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-        <!-- CSS Template -->
+        <!-- CSS Template and Others-->
         <link rel="stylesheet" href="<?=URL?>/public/css/template.css">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
     </head>
-    <body>
+    <style>
+        body{background:url('<?=URL?>/public/img/bg/bg-login.jpg')no-repeat;-webkit-background-size:cover;-moz-background-size:cover;-o-background-size:cover;background-size:cover}
+        #right{float: right;}.card{border-radius: 0;height: 87.8vh;}#form-login{padding-top: 25px;}
+    </style>
+    <body style="font-family: 'Niramit', sans-serif;">
 
         <?php include 'app/Views/templates/navbar.php' ?>
 
-
+        <div id="right" class="col-4 p-0">
+            <div class="card bg-light">
+                <div class="card-body">
+                    <h2 class="text-center pt-4">
+                        Acessar Painel
+                    </h2>
+                    <div class="text-center mt-4">
+                        <a href="cadastro">Cadastrar meu Restaurante</a>
+                    </div>
+                    <form action="" method="post" id="form-login" class="mx-4">
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="email" name="email" id="email" class="form-control form-template" placeholder="Seu Email" aria-describedby="error_email">
+                            <small style="float: right" id="error_email" class="text-danger"></small>
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="password">Senha:</label>
+                            <input type="password" name="password" id="password" class="form-control form-template" placeholder="Sua Senha" aria-describedby="error_password">
+                            <small style="float: right" id="error_password" class="text-danger"></small>
+                        </div>
+                        <input type="hidden" name="action" value="logar">
+                        <div class="form-group">
+                            <button type="button" name="btn-login" id="btn-login" class="btn btn-primary btn-lg btn-block btn-template">
+                                Entrar
+                            </button>
+                        </div>
+                        <div class="form-group text-center">
+                            <a class="text-danger" href="#">
+                                <i class="fas fa-lock"></i>
+                                Recuperar Senha
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
         
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
