8e9263049b6d8736c795bc160511d57e65c440ab
Danilo Gonçalves <danilodesenvolvedorweb@hotmail.com>
2018-11-20 15:16:47 -0200

    Font Family => Niramit

--- app/Views/error/404.php
+++ app/Views/error/404.php
@@ -9,7 +9,7 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     </head>
-    <body>
+    <body style="font-family: 'Niramit', sans-serif;">
         <h1 class="text-center mt-5 pt-5">404. Página não encontrada!</h1>        
     </body>
 </html>
--- app/Views/restaurante/cadastro.php
+++ app/Views/restaurante/cadastro.php
@@ -22,7 +22,7 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
     </head>
-    <body>
+    <body style="font-family: 'Niramit', sans-serif;">
 
         <?php include 'app/Views/templates/navbar.php' ?>
         
